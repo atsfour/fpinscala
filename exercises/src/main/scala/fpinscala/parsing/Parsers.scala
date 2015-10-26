7b89bb77a6fe7bbafae0d4b1d69f14a0755ad3b7
atsfour <atsfour@users.noreply.github.com>
2015-10-27 00:42:07 +0900

    exercise 9.1

--- exercises/src/main/scala/fpinscala/parsing/Parsers.scala
+++ exercises/src/main/scala/fpinscala/parsing/Parsers.scala
@@ -5,33 +5,115 @@ import scala.util.matching.Regex
 import fpinscala.testing._
 import fpinscala.testing.Prop._
 
-trait Parsers[Parser[+_]] { self => // so inner classes may call methods of trait
+trait Parsers[Parser[+ _]] {
+  self =>
+  // so inner classes may call methods of trait
+
+  implicit def string(s: String): Parser[String]
+
+  implicit def operators[A](p: Parser[A]): ParserOps[A] = ParserOps[A](p)
+
+  implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]):
+  ParserOps[String] = {
+    ParserOps(f(a))
+  }
+
+  def or[A](p1: Parser[A], p2: Parser[A]): Parser[A]
+
+  def run[A](p: Parser[A])(input: String): Either[ParseError, A]
+
+  def slice[A](p: Parser[A]): Parser[String]
+
+  def succeed[A](a: A): Parser[A] = string("") map (_ => a)
+
+  def product[A,B](p1: Parser[A], p2: Parser[B]): Parser[(A, B)]
+
+  def char(c: Char): Parser[Char] = {
+    string(c.toString) map (_.charAt(0))
+  }
+
+  def many[A](p: Parser[A]): Parser[List[A]]
+
+  def map[A, B](p: Parser[A])(f: A => B): Parser[B]
+
+  def map2[A, B, C](p1: Parser[A], p2: Parser[B])(f: (A, B) => C): Parser[C] = {
+    product(p1, p2).map{case (a, b) => f(a, b)}
+  }
+
+  def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]]
 
   case class ParserOps[A](p: Parser[A]) {
+    def |[B >: A](p2: Parser[B]): Parser[B] = {
+      self.or(p, p2)
+    }
 
+    def or[B >: A](p2: Parser[B]): Parser[B] = {
+      self.or(p, p2)
+    }
+
+    def map[B](f: A => B): Parser[B] = self.map(p)(f)
+
+    def many: Parser[List[A]] = self.many(p)
+
+    def slice: Parser[String] = self.slice(p)
+
+    def product[B](p2: Parser[B]): Parser[(A, B)] = self.product(p, p2)
+
+    def **[B](p2: Parser[B]): Parser[(A, B)] = self.product(p, p2)
 
   }
 
   object Laws {
+    def equal[A](p1: Parser[A], p2: Parser[A])(in: Gen[String]): Prop = {
+      forAll(in)(s => run(p1)(s) == run(p2)(s))
+    }
+
+    def mapLaw[A](p: Parser[A])(in: Gen[String]): Prop = {
+      equal(p, p.map(a => a))(in)
+    }
+
+    def charLaw(in: Gen[Char]): Prop = {
+      forAll(in)(c => run(char(c))(c.toString) == Right(c))
+    }
+
+    def stringLaw(in: Gen[String]): Prop = {
+      forAll(in)(s => run(s)(s) == Right(s))
+    }
+
+    def orSpec: Prop = {
+      check(run("abra" | "cadabra")("abra") == Right("abra")) &&
+        check(run("abra" | "cadabra")("cadabra") == Right("cadabra"))
+    }
+
+    def listOfNSpec: Prop = {
+      check(run(listOfN(3, "ab" | "cad"))("ababcad") == Right("ababcad")) &&
+        check(run(listOfN(3, "ab" | "cad"))("cadabab") == Right("cadabab")) &&
+        check(run(listOfN(3, "ab" | "cad"))("ababab") == Right("ababab"))
+    }
+
+    def succeedLaw[A](in: Gen[A]): Prop = {
+      forAll(in)(a => run(succeed(a))("") == Right(a))
+    }
   }
+
 }
 
 case class Location(input: String, offset: Int = 0) {
 
-  lazy val line = input.slice(0,offset+1).count(_ == '\n') + 1
-  lazy val col = input.slice(0,offset+1).reverse.indexOf('\n')
+  lazy val line = input.slice(0, offset + 1).count(_ == '\n') + 1
+  lazy val col = input.slice(0, offset + 1).reverse.indexOf('\n')
 
   def toError(msg: String): ParseError =
     ParseError(List((this, msg)))
 
-  def advanceBy(n: Int) = copy(offset = offset+n)
+  def advanceBy(n: Int) = copy(offset = offset + n)
 
   /* Returns the line corresponding to this location */
-  def currentLine: String = 
-    if (input.length > 1) input.lines.drop(line-1).next
+  def currentLine: String =
+    if (input.length > 1) input.lines.drop(line - 1).next
     else ""
 }
 
-case class ParseError(stack: List[(Location,String)] = List(),
+case class ParseError(stack: List[(Location, String)] = List(),
                       otherFailures: List[ParseError] = List()) {
 }
